d05741de428ae33833c10fe51db5f869199f4bde
Siddig Zeidan <siddigie1234@gmail.com>
2016-06-03 04:00:41 +0100

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,46 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { App } from './App';
+var React = require('react'),
+        _ = require('lodash'),
+        d3 = require('d3');
 
-render(<App />, document.getElementById('root'));
+
+var H1BGraph = React.createClass({
+  componentWillMount: function () {
+    this.loadRawData();
+  },
+
+  getInitialState: function () {
+    return {rawData: []};
+  },
+
+  loadRawData: function (){
+  
+    d3.csv(this.props.url)
+      .get(function (error, rows){
+        if(error) {
+          console.error(error);
+          console.error(error.stack);
+        }else{
+          this.setState({rawData: rows});
+        }
+      }.bind(this));
+  },
+
+  render: function() {
+    return (
+      <div className='row'>
+        <div className='col-md-12'>
+            <svg width='700'
+                height = '500'>
+            </svg>
+        </div>
+      </div>
+    )
+  }
+})
+
+
+React.render(
+  <H1BGraph url ="data/h1bs.csv" />,
+  document.querySelectorAll('.h1bgraph')
+  [0]
+);
