54ab7e90817cafd5bfe4482d612c1ebddffa7700
Siddig Zeidan <siddigie1234@gmail.com>
2016-06-06 22:51:01 +0100

    cleaner d3 and react approach

--- src/drawers.jsx
+++ src/drawers.jsx
@@ -2,6 +2,22 @@ var React = require('react'),
     d3 = require('d3');
 
 var Histogram = React.createClass({
+    componentWillMount: function() {
+      this.histogram = d3.layout.histogram();
+      this.widthScale = d3.scale.linear();
+      this.yScale = d3.scale.linear();
+
+      this.update_d3(this.props);
+    },
+
+    componentWillReceiveProps: function (newProps) {
+      this.update_d3(newProps);
+    },
+
+    update_d3: function (props) {
+
+    },
+    
     render: function () {
       var translate = "translate(0,
       "+this.props.topMargin+")";
